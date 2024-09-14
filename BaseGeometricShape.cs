0207cce7590327b016be414e0fdb3d9908467316
tugba <tugba@DESKTOP-UVFK9K1>
2024-09-14 13:24:51 +0300

    Add project files.

---
+++ BaseGeometricShape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PolymorphismProject
+{
+    public class BaseGeometricShape // Base class, BaseGeometricShape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public virtual int CalculateArea() // Virtual method, CalculateArea
+        { 
+            return Width * Height;
+        }
+    }
+     
+    public class Rectangle : BaseGeometricShape // Rectangle inherited from BaseGeometricShape
+    {      
+    }
+
+    public class  Square : BaseGeometricShape // Square inherited from BaseGeometricShape
+    {
+    }
+
+    public class RighTriangle : BaseGeometricShape // RightTriangle inherited from BaseGeometricShape
+    {
+        public override int CalculateArea() // Override method
+        {
+            return base.CalculateArea() / 2;
+        }
+    }
+}
---
+++ Polymorphism.cs
@@ -0,0 +1,17 @@
+﻿using PolymorphismProject;
+
+BaseGeometricShape rectangle = new Rectangle(); // Created a Rectangle object of type BaseGeometripShape
+rectangle.Width = 5;
+rectangle.Height = 5;
+Console.WriteLine(rectangle.CalculateArea()); // Rectangle area calculation method printed
+
+BaseGeometricShape square = new Square(); // Created a Square object of type BaseGeometripShape
+square.Width = 10;
+square.Height = 10;
+Console.WriteLine(square.CalculateArea()); // Square area calculation method printed
+
+BaseGeometricShape triangle = new RighTriangle(); // Created a RightTriangle object of type BaseGeometripShape
+triangle.Width = 5;
+triangle.Height = 2;
+Console.WriteLine(triangle.CalculateArea()); // Right Triangle area calculation method printed
+
